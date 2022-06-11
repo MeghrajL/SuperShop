57e445c7e441a57fbc7aea3c4464cc2b1e4a32fb
MeghrajL <meghrajlot@gmail.com>
2022-06-11 17:49:35 +0530

    frontend v1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,26 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import HomeScreen from "./Screens/HomeScreen";
+import ProductScreen from "./Screens/ProductScreen";
+import { Container } from "react-bootstrap";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
 function App() {
   return (
-    <div className="App">
-      <h1>Super Shop</h1>
-    </div>
+    <Router>
+      <Header />
+      <main className="py-3">
+        {" "}
+        {/*top padding 3*/}
+        <Container>
+          <Routes>
+            <Route path="/" element={<HomeScreen />} exact />
+            <Route path="/product/:id" element={<ProductScreen />} />
+          </Routes>
+        </Container>
+      </main>
+      <Footer />
+    </Router>
   );
 }
 
